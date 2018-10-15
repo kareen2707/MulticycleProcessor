1e746a845a086b98e75eff333ad1db73f34377bf
LAPTOP-TATU00RO\kareen <kareen2707@gmail.com>
2018-10-15 10:32:01 +0200

    Final version of multiplexer.vhd

--- template_lab1/vhdl/multiplexer.vhd
+++ template_lab1/vhdl/multiplexer.vhd
@@ -13,5 +13,34 @@ entity multiplexer is
 end multiplexer;
 
 architecture synth of multiplexer is
+
+signal s_i0: std_logic_vector(31 downto 0);
+signal s_i1: std_logic_vector(31 downto 0);
+signal s_i2: std_logic_vector(31 downto 0);
+signal s_i3: std_logic_vector(31 downto 0);
+signal s_sel: std_logic_vector(1 downto 0);
+signal s_o: std_logic_vector(31 downto 0);
+
 begin
+
+s_i0 <= i0;
+s_i1 <= i1;
+s_i2 <= i2;
+s_i3 <= i3;
+s_sel <= sel;
+o <= s_o;
+
+	multiplexer_process : process(s_i0, s_i1, s_i2, s_i3, s_sel) 
+	begin
+		case s_sel is 
+		
+			when "00" => s_o <= s_i0;
+			when "01" => s_o <= s_i1;
+			when "10" => s_o <= s_i2;
+			when "11" => s_o <= s_i3;
+			when others => s_o <= s_i0;
+		end case;
+
+	end process;
 end synth;
+
