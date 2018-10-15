4d367c75fb18fc760b376d6d7697a8f0867f74e1
LAPTOP-TATU00RO\kareen <kareen2707@gmail.com>
2018-10-15 10:27:53 +0200

    Final version of comparator.vhd

--- template_lab1/vhdl/comparator.vhd
+++ template_lab1/vhdl/comparator.vhd
@@ -14,5 +14,37 @@ entity comparator is
 end comparator;
 
 architecture synth of comparator is
+
+signal s_a_31: std_logic;
+signal s_b_31: std_logic;
+signal s_diff_31: std_logic;
+signal s_carry: std_logic;
+signal s_zero: std_logic;
+signal s_op: std_logic_vector(2 downto 0);
+signal s_r: std_logic;
+signal s_temp0: std_logic;
+signal s_temp1: std_logic;
+signal s_temp2: std_logic;
+
 begin
+
+s_a_31 <= a_31;
+s_b_31 <= b_31;
+s_diff_31 <= diff_31;
+s_carry <= carry;
+s_zero <= zero;
+s_op <= op;
+r <= s_r;
+
+with s_op select
+	s_r <=
+	(s_a_31 and not s_b_31) or ( (s_a_31 xnor s_b_31) and (s_diff_31 or s_zero)) when "001",
+	(s_b_31 and not s_a_31) or ((s_a_31 xnor s_b_31) and (not s_diff_31 and not s_zero)) when "010",
+	not(s_zero) when "011",
+	s_zero when "100",
+	(not(s_carry)) or s_zero when "101",
+	s_carry and (not(s_zero)) when "110",
+	s_zero when others;
+
+
 end synth;
